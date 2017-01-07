423e8d83e95844b475c7e5d9373d8151492386c9
argshook <argshook@gmail.com>
2017-01-07 16:04:14 +0200

    i3status: less spacing for current playing audio

--- .config/i3status/i3status-custom.sh
+++ .config/i3status/i3status-custom.sh
@@ -2,10 +2,10 @@
 # shell script to prepend i3status with cmus song and artist
 i3status | while :
 do
-        read line
-        artist=$(cmus-remote -Q | grep ' artist ' | cut -d ' ' -f3-)
-        song=$(cmus-remote -Q | grep title | cut -d ' ' -f3-)
-        stream=$(cmus-remote -Q | grep stream | cut -d ' ' -f2-)
-        echo "  $stream            $line" || exit 1
+  read line
+  artist=$(cmus-remote -Q | grep ' artist ' | cut -d ' ' -f3-)
+  song=$(cmus-remote -Q | grep title | cut -d ' ' -f3-)
+  stream=$(cmus-remote -Q | grep stream | cut -d ' ' -f2-)
+  echo "  $stream | $line" || exit 1
 done
 
