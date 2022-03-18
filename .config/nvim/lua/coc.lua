8d932b5414e92d237bf5a80319236b5295274cd2
argshook <argshook@gmail.com>
2022-03-18 10:21:05 +0200

    udpate

--- .config/nvim/lua/coc.lua
+++ .config/nvim/lua/coc.lua
@@ -1,5 +1,10 @@
+local utils = require('map_utils')
+local lua_fn = utils.lua_fn
+local key = vim.api.nvim_set_keymap
+
 vim.g.coc_global_extensions = {
   'coc-eslint',
+  'coc-prettier',
   'coc-tsserver',
   'coc-pyright',
   'coc-lua',
@@ -17,22 +22,20 @@ local function show_documentation()
   end
 end
 
-local set = vim.keymap.set
+key('n', '[l', '<Plug>(coc-diagnostic-prev)', {})
+key('n', ']l', '<Plug>(coc-diagnostic-next)', {})
+key('n', '<leader>rn', '<Plug>(coc-rename)' , {})
+key('n', 'gd', '<Plug>(coc-definition)', { silent = true })
+key('n', 'gD', '<Plug>(coc-declaration)', { silent = true })
+key('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
+key('n', 'gy', '<Plug>(coc-diagnostic-info)', { silent = true })
+key('n', 'gr', '<Plug>(coc-references)', { silent = true })
+key('n', 'go', ':CocList outline<cr>', { silent = true })
+key('n', '<leader>qf', '<Plug>(coc-fix-current)', {})
+key('n', '<leader>ac', '<Plug>(coc-codeaction)', {})
+key('n', 'K', lua_fn(function() show_documentation() end), {})
 
-set('n', '[l', '<Plug>(coc-diagnostic-prev)')
-set('n', ']l', '<Plug>(coc-diagnostic-next)')
-set('n', '<leader>rn', '<Plug>(coc-rename)')
-set('n', 'gd', '<Plug>(coc-definition)', { silent = true })
-set('n', 'gD', '<Plug>(coc-declaration)', { silent = true })
-set('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
-set('n', 'gy', '<Plug>(coc-diagnostic-info)', { silent = true })
-set('n', 'gr', '<Plug>(coc-references)', { silent = true })
-set('n', 'go', ':CocList outline<cr>', { silent = true })
-set('n', '<leader>qf', '<Plug>(coc-fix-current)')
-set('n', '<leader>ac', '<Plug>(coc-codeaction)')
-set('n', 'K', function() show_documentation() end)
-
-set('i', '<c-space>', 'coc#refresh()', { silent = true, expr = true })
+key('i', '<c-space>', 'coc#refresh()', { silent = true, expr = true })
 
 vim.cmd([[
 autocmd CursorHold * silent call CocActionAsync('highlight')
